eb83edc1cd46327c6070c02f34fcd6d2bdcdb67f
Nicholai <nichobech02@gmail.com>
2023-11-10 12:39:47 +0100

    First commit

---
+++ my-project/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+
+import "./index.css";
+
+const = App() => {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    </div>
+  );
+}
+
+export default App;
